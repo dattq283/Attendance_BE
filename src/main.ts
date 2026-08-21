dc0ea0b1e8ed73fa94e19acaf236724745c12385
Dat <trinhquocdat2803@gmail.com>
2026-08-21 08:49:34 +0700

    Fix:Enable validationPipe globally- None of the input data validations are running.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 //import { RedisIoAdapter } from './redis-io.adapter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   // const redisIoAdapter = new RedisIoAdapter(app);
   // await redisIoAdapter.connectToRedis();
   // app.useWebSocketAdapter(redisIoAdapter);
