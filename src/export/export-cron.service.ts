f4d1516a19befad71534913e5e8241970e47b612
Dat <trinhquocdat2803@gmail.com>
2026-09-14 10:46:44 +0700

    Fix: timezone of cron to Aisa/Ho_Chi_Minh

--- src/export/export-cron.service.ts
+++ src/export/export-cron.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { ExportService } from './export.service';
-
+import { businessTime } from '../utils/date.util';
 @Injectable()
 export class ExportCronService {
   constructor(private readonly exportService: ExportService) {}
-  @Cron(CronExpression.EVERY_1ST_DAY_OF_MONTH_AT_MIDNIGHT)
+  @Cron(CronExpression.EVERY_1ST_DAY_OF_MONTH_AT_MIDNIGHT, {
+    timeZone: 'Asia/Ho_Chi_Minh',
+  })
   async trgMonthlyExport() {
-    const now = new Date();
-    const month = now.getMonth() === 0 ? 12 : now.getMonth();
-    const year =
-      now.getMonth() === 0 ? now.getFullYear() - 1 : now.getFullYear();
-
-    console.log(`Start export ${month}/${year}`);
-    await this.exportService.trgMonthlyExport(month, year);
+    const { year, month } = businessTime(new Date());
+    const prevYear = month === 1 ? year - 1 : year;
+    const prevMonth = month === 1 ? 12 : month - 1;
+    console.log(`Start export ${prevMonth}/${prevYear}`);
+    await this.exportService.trgMonthlyExport(prevMonth, prevYear);
   }
 }
